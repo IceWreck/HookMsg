7780427e1e9ab5f0fbcce3da52145d15b1ad2590
Anchit Bajaj <ab@abifog.com>
2021-02-26 00:49:37 +0530

    Experimental messages to matrix

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 run:
 	go run ./cmd/hookmsg
 
+run-sample:
+	go run ./cmd/sample
+
 build:
 	go build -o ./bin/HookMsg ./cmd/hookmsg
--- cmd/sample/main.go
+++ cmd/sample/main.go
@@ -2,6 +2,25 @@
 
 package main
 
+import (
+	"log"
+
+	"github.com/IceWreck/HookMsg/config"
+	matrix "github.com/matrix-org/gomatrix"
+)
+
 func main() {
+	client, _ := matrix.NewClient(config.Config.MatrixHomeserver, "", "")
+	resp, err := client.Login(&matrix.ReqLogin{
+		Type:     "m.login.password",
+		User:     config.Config.MatrixUserName,
+		Password: config.Config.MatrixPassword,
+		DeviceID: config.Config.MatrixDeviceID,
+	})
+	if err != nil {
+		log.Fatal("err logging in", err)
+	}
+	client.SetCredentials(resp.UserID, resp.AccessToken)
+	client.SendText(config.Config.MatrixChannels["security-info"].ID, "lol u")
 
 }
